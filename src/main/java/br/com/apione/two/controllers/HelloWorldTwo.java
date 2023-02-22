7e84059b43c87c50219d78507f327266d19938c8
gabriel <gabrielalmeidads@gmail.com>
2023-02-21 21:10:08 -0300

    Update application.properties and adding controller for other service

--- src/main/java/br/com/apione/two/controllers/HelloWorldTwo.java
+++ src/main/java/br/com/apione/two/controllers/HelloWorldTwo.java
@@ -1,17 +1,32 @@
 package br.com.apione.two.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping(value = "/hello")
+@RequestMapping(value = "/apitwo")
 public class HelloWorldTwo {
-
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<String> findAll() {
-		return ResponseEntity.ok().body("Hello world!");
+		return ResponseEntity.ok().body("Hello world two!");
+	}
+	
+	@RequestMapping(value = "/hello2", method = RequestMethod.GET)
+	public ResponseEntity<String> findAll2() {
+		String res = restTemplate.getForObject("http://apione-service/hello/", String.class);
+		return ResponseEntity.ok().body(res);
+		
 	}
 	
 }
+
